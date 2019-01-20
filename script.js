9a4efc2eb85d9a9871bf5c08e038848ba73e97d2
xtrp <36894883+xtrp@users.noreply.github.com>
2019-01-20 10:23:07 +0000

    finished beta

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var urlVariable = window.location.search.substring(1);
+if(urlVariable != ""){
+   firebase.database().ref(urlVariable).once("value", function(snapshot){
+      if(snapshot.val()){
+         window.open(snapshot.val(), "_self");
+      }
+   });
+}
+
 function createRandomString(){
    var possibleChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
    var returnString = "";
